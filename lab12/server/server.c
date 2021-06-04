3cc763b2f53f6b52ef4604240e919892e20730bf
NewExample <60753928+NewExample@users.noreply.github.com>
2021-06-04 15:00:47 +0300

    Update server.c

--- lab12/server/server.c
+++ lab12/server/server.c
@@ -29,20 +29,20 @@ void print_help()
 int main(int argc, char* argv[])
 {
     int port = atoi(argv[1]);
-    struct sockaddr_in server_addr;
+    struct sockaddr_in server_addr; 
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(port); //cброс структуры сервера
 
-    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    int sock = socket(PF_INET, SOCK_STREAM, 0);//создание сокета
     if (sock < 0)
     {
         perror("Socket error");
         return -1;
     }
-    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)//bind чтобы сервер получил ip по порту
     {
         server_addr.sin_port = 0;
         if (bind(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
@@ -53,8 +53,8 @@ int main(int argc, char* argv[])
     }
 
     socklen_t server_len = sizeof(server_addr);
-    listen(sock, 100);
-    getsockname(sock, (struct sockaddr *)&server_addr, &server_len);
+    listen(sock, 100);//позволяет подключаться клиентам
+    getsockname(sock, (struct sockaddr *)&server_addr, &server_len);//получаем имя сервера
     printf("Listening on port: %d\n", ntohs(server_addr.sin_port));
 
     if (fork() == 0)
@@ -62,8 +62,8 @@ int main(int argc, char* argv[])
         while (1)
         {
             struct sockaddr_in client_addr;
-            socklen_t client_len = sizeof(client_addr);
-            int newSock = accept(sock, (struct sockaddr *)&client_addr, &client_len);
+            socklen_t client_len = sizeof(client_addr);//обьявляем структуру сервера клиента
+            int newSock = accept(sock, (struct sockaddr *)&client_addr, &client_len);//до тех пор пока клиент не подключться. потом идёт получение сокета
             
             if (fork() == 0)
             {
@@ -72,14 +72,14 @@ int main(int argc, char* argv[])
             while (1)
             {
                 char input[128];
-                int n = read(newSock, input, sizeof(input));
+                int n = read(newSock, input, sizeof(input));//читает с сокета
                 if (n == 0)
                 {
-                    close(newSock);
+                    close(newSock);//если не пришёл пакет то зыкрываем сокет
                     exit(0);
                 }
 
-                FILE* file = fopen(input, "r");
+                FILE* file = fopen(input, "r");// создаём файл. Открываекм файл
                 if (!file)
                 {
                     long fileSize = -1 ;
@@ -87,19 +87,19 @@ int main(int argc, char* argv[])
                 }
                 else
                 {
-                    long fileSize = getFileSize(file);
-                    write(newSock, &fileSize, sizeof(fileSize));
+                    long fileSize = getFileSize(file); // если открыли то получаем размер файла
+                    write(newSock, &fileSize, sizeof(fileSize));//читает размер файла
                     char message[fileSize];
-                    fread(message, sizeof(char), fileSize, file);
+                    fread(message, sizeof(char), fileSize, file);// читает файл и запоминает в буфер 
                     fclose(file);
-                    write(newSock, message, sizeof(message));
+                    write(newSock, message, sizeof(message));// записывает по сокету клиента сообщение
                 }
             }
         }
     }
     else
     {
-        print_help();
+        print_help();// обработка команд сервера
         char message[128];
         while (1)
         {
